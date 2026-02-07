1a2e9847f485cade7f579e91e2ba9840d523546e
eshen <etiennewayne@gmail.com>
2026-02-07 12:34:54 +0800

    added student controller

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,38 @@
+<?php
+
+
+class StudentController {
+    //
+
+
+
+
+
+    public function store(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function destroy(){
+    }
+
+
+
+
+
+
+
+    //
+
+
+
+
+
+
+
+
+    //
+}
